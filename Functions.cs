9e0742ac3672f58e255da01b5b3519412045e509
AlexanderMATI <alvander99@mail.ru>
2020-03-21 13:23:04 +0300

    Initial commit

---
+++ Functions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace laba05
+{
+    class Functions
+    {
+        public static string systemFrac(decimal fraction, int system)
+        {
+            var fractionList = new List<decimal>();
+            var result = String.Empty;
+            int i = 0;
+            while (fraction >= (decimal)0.001 && i++ <= 100)
+            {
+                if (fractionList.Contains(fraction))
+                {
+                    var temp = result.Substring(fractionList.IndexOf(fraction));
+                    result = result.Substring(0, fractionList.IndexOf(fraction));
+                    result += "(" + temp + ")";
+                    break;
+                }
+                fractionList.Add(fraction);
+                fraction *= system;
+                if (Math.Truncate(fraction) <= 9)
+                    result += (char)(Math.Truncate(fraction) + '0');
+                else if (Math.Truncate(fraction) >= 10)
+                    result += (char)(Math.Truncate(fraction) + 'A' - 10);
+                fraction -= Math.Truncate(fraction);
+            }
+            return result;
+        }
+        public static string NumtoSystem(int number, decimal fraction, int system)
+        {
+            List<char> iList = new List<char>();
+           
+            while ((number / system) != 0)
+            {
+                if (number % system > 9)
+                    iList.Add((char)(number % system + 'A' - 10));
+                else
+                    iList.Add((char)(number % system + '0'));
+                number /= system;
+            }
+
+            if (number % system > 9)
+                iList.Add((char)(number % system + 'A' - 10));
+            else
+                iList.Add((char)(number % system + '0'));
+
+            iList.Reverse();
+            var res = String.Empty;
+            res = String.Concat(iList);
+            res += ',';
+            res += systemFrac(fraction, system);
+            return res;
+        }
+
+        
+    }
+}
---
+++ laba05.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace laba05
+{
+    class laba05
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Launch the console? Y/N");
+            ConsoleKeyInfo change = Console.ReadKey() ;
+            string tempString = string.Empty; 
+
+            if (change.Key == ConsoleKey.N)
+            {
+
+                string filename = string.Empty;
+                while (true)
+                {
+                    Console.WriteLine("Enter name of your file after his path: ");
+                filename = Console.ReadLine();
+
+                    if (!File.Exists(filename))
+                    {
+                        Console.WriteLine("File does not exists. Repeat enter? Y/N");
+
+                        change = Console.ReadKey();
+                        if (change.Key == ConsoleKey.Y)
+                            continue;
+                        else return;
+                        
+                         }
+                    break;
+                    }//end while
+                
+
+                try
+                {
+                    using (StreamReader readerFile = new StreamReader(filename))
+                    {
+                        tempString = readerFile.ReadLine();
+
+                    }
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine("Error until reading file: " + e.Message);
+                    Console.ReadKey();
+                    return;
+                }
+            }
+
+
+
+            else if (change.Key == ConsoleKey.Y)
+            {
+
+                while (true)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Enter the number and number system");
+                    tempString = Console.ReadLine();
+                    if (tempString.Length == 0)
+                    {
+                        Console.WriteLine("Enter is empty, finish? Y/*AnySymbol");
+                        change = Console.ReadKey();
+                        if (change.Key == ConsoleKey.Y)
+                            return;
+                        else continue;
+                    }
+                    break;
+                }//end while
+            }
+            else return;
+            var Array = tempString.Split(' ', ',');
+            Array[1] = "0," + Array[1];
+            Boolean okey = true;
+
+            var numList = new List<decimal>();
+            if (Array.Length == 3)
+            {
+                for (int i = 0; i < Array.Length; i++)
+                {
+                    decimal num;
+                    okey = decimal.TryParse(Array[i], out num);
+                    if (okey)
+                    {
+                        numList.Add(num);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong symbols");
+                        Console.ReadKey();
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error empty");
+                Console.ReadKey();
+                return;
+            }
+            if (numList[2] >= 2)
+            {
+                Console.WriteLine(Functions.NumtoSystem((int)numList[0], numList[1], (int)numList[2]));
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Error empty");
+                Console.ReadKey();
+                return;
+            }
+        }
+    }
+}
